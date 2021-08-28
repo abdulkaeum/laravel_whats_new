7d2c2550600964df98ad76830ad7e33ef6f7d895
abdulkaeum <abdul.kaeum@hotmail.com>
2021-08-28 21:03:48 +0100

    New Error Page

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     // reduces memory consumption so server can handle load
     // not loading it all into memory
     // no queries are run here
-    $users = App\Models\User::cursor();
+    $users = App\Model\User::cursor();
 
     // no queries are run until you pull it out from the lazy collection returned as per below
     dd($users->first()->name);
