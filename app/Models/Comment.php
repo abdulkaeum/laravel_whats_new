4a98d1473ceec5f5935d92d9a7efc199a0359753
abdulkaeum <abdul.kaeum@hotmail.com>
2021-08-28 14:03:42 +0100

    caching TTL to seconds

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,6 +11,13 @@ class Comment extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        // caching was in min before but now done in seconds
+        // to cache something for 1 hour = 60 min
+        // now 3600 seconds
+        return cache()->remember('key', 3600, function () {
+            return $this->belongsTo(User::class);
+        });
+
+        // you could use the now()-addHour() which will be converted to seconds
     }
 }
