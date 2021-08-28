fcc7273be0a3028c66a91d12c16cfa8e6ee9c533
abdulkaeum <abdul.kaeum@hotmail.com>
2021-08-28 20:02:59 +0100

    Lazy Collections

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //if you were to have a massive set of data  User::all() will crash
+    // this will run x many records query plus x many collections all at once
+    //$users = App\Models\User::all();
+
+    // lazy collection returns a generator
+    // reduces memory consumption so server can handle load
+    // not loading it all into memory
+    // no queries are run here
+    $users = App\Models\User::cursor();
+
+    // no queries are run until you pull it out from the lazy collection returned as per below
+    dd($users->first()->name);
 });
